046ce99cda1399d1c4b60be4b696b6e5c0a6b278
tf63 <74246282+tf63@users.noreply.github.com>
2025-12-07 01:11:55 +0900

    feat: add tsconfig.build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	// output: "standalone",
+	devIndicators: false,
+	typescript: {
+		tsconfigPath: "tsconfig.build.json",
+	},
 }
 
 export default nextConfig
