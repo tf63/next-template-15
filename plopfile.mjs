5b864b3a629e771dd8f2ed9aa796375e377b5d75
tf63 <74246282+tf63@users.noreply.github.com>
2025-12-07 23:37:30 +0900

    feat: update plopfile

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,31 +1,49 @@
 import path from "node:path"
 
+function extractFilename(filePath) {
+	return path.basename(filePath)
+}
+
+function extractComponentPrefix(filePath) {
+	if (!filePath.includes("/")) {
+		return ""
+	}
+	return filePath.split("/")[0]
+}
+
 export default (plop) => {
-	plop.setHelper("extractFilename", (filePath) => path.basename(filePath))
+	plop.setHelper("extractFilename", extractFilename)
+	plop.setHelper("extractComponentPrefix", extractComponentPrefix)
 
 	plop.setGenerator("component", {
 		description: "Create a new component",
 		prompts: [
 			{
+				type: "list",
+				name: "type",
+				message: "Please select the component type",
+				choices: ["src/features", "src/components"],
+			},
+			{
 				type: "input",
 				name: "path",
-				message: "Please enter the component path (e.g. features/sample/sample-button)",
+				message: "Please enter the component path (e.g. sample/sample-button)",
 			},
 		],
 		actions: [
 			{
 				type: "add",
-				path: "src/{{path}}/{{kebabCase (extractFilename path)}}.tsx",
+				path: "{{type}}/{{path}}/{{kebabCase (extractFilename path)}}.tsx",
 				templateFile: "templates/components/component.tsx.hbs",
 			},
 			{
 				type: "add",
-				path: "src/{{path}}/{{kebabCase (extractFilename path)}}.spec.tsx",
+				path: "{{type}}/{{path}}/{{kebabCase (extractFilename path)}}.spec.tsx",
 				templateFile: "templates/components/component.spec.tsx.hbs",
 			},
 			{
 				type: "add",
-				path: "src/{{path}}/{{kebabCase (extractFilename path)}}.stories.tsx",
+				path: "{{type}}/{{path}}/{{kebabCase (extractFilename path)}}.stories.tsx",
 				templateFile: "templates/components/component.stories.tsx.hbs",
 			},
 		],
