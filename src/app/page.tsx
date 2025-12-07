51f1536d686677084f98b1668adfefe6d4898069
tf63 <74246282+tf63@users.noreply.github.com>
2025-12-07 15:52:06 +0900

    fix: tailwind settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { SampleBlock } from "../features/sample/sample-block/sample-block"
+
 export default function Page() {
 	return (
 		<main className="space-y-4 p-4">
 			<h1 className="text-2xl font-bold">Hello, Next.js!</h1>
 			<p className="text-foreground">This is a sample page using Tailwind CSS.</p>
+
+			<SampleBlock />
 		</main>
 	)
 }
